b845e683aa73b9fcd2b4c9e18ae59b3e56fbcc9e
Abdimalik11 <ilkacaze5522@gmail.com>
2023-06-27 12:18:04 -0700

    this is the answer of capitalize-letters

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 // Good Luck 💪🏾
+function makeCapital(str) {
+    let words = str.split(" ");
+    for (let i = 0; i < words.length; i++) {
+ 
+      words[i] = words[i][0].toUpperCase() + words[i].substring(1);
+    }
+
+    return words.join(" ");
+  }
+  console.log(makeCapital("my name is ali"));
+  
+  console.log(makeCapital("fikrcamp Bootcamp"));
+  
+  console.log(makeCapital("i live In Paris"));
